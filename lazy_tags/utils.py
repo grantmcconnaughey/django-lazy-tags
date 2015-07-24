a6f24717e0cd279306479f56a8433222dca42281
Grant McConnaughey <grantmcconnaughey@gmail.com>
2015-07-23 21:53:19 -0500

    Fix Django/Python compat issues

--- lazy_tags/utils.py
+++ lazy_tags/utils.py
@@ -8,13 +8,13 @@ def get_tag_html(tag, args=None, kwargs=None):
     if args:
         args = json.loads(args)
         for arg in args:
-            args_str += "'{}'".format(arg)
+            args_str += "'{0}'".format(arg)
 
     kwargs_str = ''
     if kwargs:
         kwargs = json.loads(kwargs)
-        for name, value in kwargs.iteritems():
-            kwargs_str += "{}='{}' ".format(name, value)
+        for name, value in kwargs.items():
+            kwargs_str += "{0}='{1}' ".format(name, value)
 
     html = '{{% load {lib} %}}{{% {tag_name} {args} {kwargs} %}}'.format(
         lib=lib, tag_name=tag_name, args=args_str, kwargs=kwargs_str)

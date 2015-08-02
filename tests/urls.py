7952b613fff10feb4334a9841ca245165496bad4
Grant McConnaughey <grantmcconnaughey@gmail.com>
2015-08-02 18:54:31 -0500

    Refactor tests

--- tests/urls.py
+++ tests/urls.py
@@ -8,7 +8,7 @@ from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.test, name='lazy_tags_jquery'),
+    url(r'^$', views.test_jquery, name='lazy_tags_jquery'),
     url(r'^js/$', views.test_javascript, name='lazy_tags_javascript'),
     url(r'^prototype/$', views.test_prototype, name='lazy_tags_prototype'),
     url(r'^lazy_tags/', include('lazy_tags.urls')),
--- tests/views.py
+++ tests/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 
-def test(request):
-    return render(request, 'tests/test.html')
+def test_jquery(request):
+    return render(request, 'tests/test_jquery.html')
 
 
 def test_javascript(request):
